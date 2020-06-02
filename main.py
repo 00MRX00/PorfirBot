9feb5f56f1423ef2009d5e43b4b9e275caa05236
00MRX00 <17515560@mail.ru>
2020-06-02 23:35:47 +0300

    refactored main.py by using OOP

--- main.py
+++ main.py
@@ -4,82 +4,83 @@ from time import sleep
 
 import misc
 
-URL = 'https://api.telegram.org/bot' + misc.token + '/'
-PROXYES = misc.proxies
 
-global lastUpdateId
-lastUpdateId = 0
-
-def getUpdates():
-    url = URL + 'getUpdates'
-    responce = requests.get(url, proxies=PROXYES)
-    return responce.json()
-
-
-def resultsToFile(fileName, text):
-    with open(f"{fileName}.json", "w", encoding="utf-8") as file:
-        json.dump(text, file, indent=4, ensure_ascii=False)
-
-
-def getMessage():
-    data = getUpdates()
-    lastObject = data["result"][-1]
-    currentUpdateId = lastObject["update_id"]
-    
-    global lastUpdateId
-    if lastUpdateId != currentUpdateId:
-        lastUpdateId = currentUpdateId
-        return {
-            "chatId": lastObject["message"]["chat"]["id"],
-            "messageText": lastObject["message"]["text"]
+class Porfirievich:
+    def __init__(self):
+        self.__URL = 'https://api.telegram.org/bot' + misc.token + '/'
+        self.__PROXYES = misc.proxies
+        self.__hystory = {
+            "lastUpdateId": 0,
+            "messages": []
         }
-    else:
-        return None
 
+    def getUpdates(self):
+        url = self.__URL + 'getUpdates'
+        responce = requests.get(url, proxies=self.__PROXYES)
+        return responce.json()
 
-def sendMessage(chatId, text="Wait a second? please..."):
-    url = URL + 'sendMessage'
-    data = {
-        "chat_id": chatId,
-        "text": text
-    }
-    requests.get(url, params=data, proxies=PROXYES)
+    def resultsToFile(self, fileName, text):
+        with open(f"{fileName}.json", "w", encoding="utf-8") as file:
+            json.dump(text, file, indent=4, ensure_ascii=False)
 
+    def getMessage(self):
+        data = self.getUpdates()
+        lastObject = data["result"][-1]
+        currentUpdateId = lastObject["update_id"]
 
-def questionHandler(question):
-    chatId = question["chatId"]
-    messageText = question["messageText"]
-    if messageText == "/porf":
-        sendMessage(chatId, "Придумайте начало истории...")
-        
-        url = "https://models.dobro.ai/gpt2/medium/"
-        message = "Привет, как дела?"
-        text = {
-            "prompt": message,
-            "num_samples": 4,
-            "length": 30
-        }
+        if self.__hystory["lastUpdateId"] != currentUpdateId:
+            self.__hystory["lastUpdateId"] = currentUpdateId
+            self.__hystory["messages"].append(lastObject)
+            return {
+                "chatId": lastObject["message"]["chat"]["id"],
+                "messageText": lastObject["message"]["text"]
+            }
+        else:
+            return None
+
+    def sendMessage(self, chatId, text="Wait a second? please..."):
+        url = self.__URL + 'sendMessage'
         data = {
             "chat_id": chatId,
-            "text": json.dumps(text)
+            "text": text
         }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        responce = requests.post(url, data=data["text"], headers=headers)
-        answer = ""
-        for i in range(3, -1, -1):
-            answer += f"[{abs(i-3)}] - {message + responce.json()['replies'][i]}\n\n"
-        sendMessage(chatId, answer)
-    else:
-        pass
+        requests.get(url, params=data, proxies=self.__PROXYES)
+
+    def questionHandler(self, question):
+        chatId = question["chatId"]
+        messageText = question["messageText"]
+        if messageText == "/porf":
+            self.sendMessage(chatId, "Придумайте начало истории...")
+
+            url = "https://models.dobro.ai/gpt2/medium/"
+            message = "Привет, как дела?"
+            text = {
+                "prompt": message,
+                "num_samples": 4,
+                "length": 30
+            }
+            data = {
+                "chat_id": chatId,
+                "text": json.dumps(text)
+            }
+            headers = {
+                "Content-Type": "application/json"
+            }
+            responce = requests.post(url, data=data["text"], headers=headers)
+            answer = ""
+            for i in range(3, -1, -1):
+                answer += f"[{abs(i-3)}] - {message + responce.json()['replies'][i]}\n\n"
+            self.sendMessage(chatId, answer)
+        else:
+            pass
 
 
 def main():
+    porf = Porfirievich()
     while True:
-        question = getMessage()
+        question = porf.getMessage()
         if question != None:
-            questionHandler(question)
+            porf.questionHandler(question)
         else:
             continue
     sleep(2)
